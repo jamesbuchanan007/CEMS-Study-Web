50cf288dfd128e02e12dbf21d31977cf81dce149
James Buchanan <jamesb@spectrumsystems.com>
2019-06-26 17:17:09 -0500

    moved 75.19 to partial view. too big for db

---
+++ CEMS Study Web/Controllers/Part75Controller.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace CEMS_Study_Web.Controllers
+{
+    public class Part75Controller : Controller
+    {
+        // GET: Part75
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+    }
+}
